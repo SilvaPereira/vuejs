0e0fe060dd29fc0ea370b170de8ca1a9f3d31666
unknown <andre_silva6@hotmail.com>
2020-06-12 18:03:29 -0300

    routing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
+
+    <router-view></router-view>
     <button v-on:click.prevent="getAllPosts">Get all Posts</button>
     <br><br>
-    <input type="text" value="">
+    <p>PostId</p>
+    <input v-model="inputPostId" type="text" id="inputPostId">
     <br><br>
-    <button v-on:click.prevent="getPost(1)">Get a Post</button>
+    <button v-on:click.prevent="getPost()">Get a Post</button>
     <br><br>
-    <button v-on:click.prevent="getPostComment(id)">Get a Post Comments</button>
+    <button v-on:click.prevent="getPostComment()">Get a Post Comments</button>
     <br><br>
     <ul>
-        <li class="list-group-item" v-for="post in posts" v-bind:key="post">{{ post.userid }} - {{ post.id }} - {{ post.title }} - {{ post.body }}</li>
+        <li class="list-group-item" v-for="post in posts" v-bind:key="post">{{ post.userId }} - {{ post.id }} - {{ post.title }} - {{ post.body }}</li>
+    </ul> 
+    <ul>
+      <li class="list-group-item">{{onePost.userId}} - {{onePost.id}} - {{onePost.title}} - {{onePost.body}}</li>
+    </ul>
+    <ul>
+        <li class="list-group-item" v-for="comment in comments" v-bind:key="comment">{{ comment.postId }} - {{ comment.id }} - {{ comment.name }} - {{ comment.email }} - {{ comment.body }}</li>
     </ul> 
   </div>
 </template>
@@ -20,11 +29,26 @@ export default {
   data() {
     return {
     post: {
-      userid : '',
+      userId : '',
       id: '',
       title: '',
       body: ''
     },
+    comment: {
+      postId : '',
+      id : '',
+      name : '',
+      email : '',
+      body : ''
+    },
+    onePost: {
+      userId : '',
+      id: '',
+      title: '',
+      body: ''
+    },
+    inputPostId: '',
+    comments: [],
     posts: []
     };
   },
@@ -43,18 +67,34 @@ export default {
                       this.posts = resultArray;
                     });
     },
-    getPost: function(id){
-      this.$http.get('posts/{{id}}')
+    getPost(){
+      this.$http.get('posts/' + inputPostId.value)
       .then(response => {  
-                      console.log(response.data);
-      });             
+                        console.log(response.data);
+                        return response.json();
+                    })
+                    .then(data => {
+                      //const resultArray = {};
+                      //for (let key in data) {
+                      //  resultArray.push(data[key]);
+                      //}
+                      this.onePost = data;
+                    });             
     },
-    getPostComment: function(id){
-      this.$http.get('posts/1/comments')
+    getPostComment(){
+      this.$http.get('posts/' + inputPostId.value + '/comments')
       .then(response => {
                         console.log(response.data);
+                        return response.json();
+                    })
+                    .then(data => {
+                      const resultComments = [];
+                      for (let key in data) {
+                        resultComments.push(data[key]);
+                      }
+                      this.comments = resultComments;
                     });
-    } 	
+    } 
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -2,19 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import VueResource from 'vue-resource'
-//import { routes } from './routes'
+import { routes } from './routes'
 
-//Vue.use(VueRouter);
+Vue.use(VueRouter);
 Vue.use(VueResource);
 
-//const router = new VueRouter({
-  //  routes 
-//});
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 Vue.http.options.root = "https://jsonplaceholder.typicode.com/"
 
 new Vue({
   el: '#app',
-  //router,
+  router,
   render: h => h(App)
 })
